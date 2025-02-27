0e25ae6f67d4e38c7b39ada146ec6a1707f7f3dc
Luke Bailey <baileylu@tcd.ie>
2025-02-27 11:06:17 +0000

    Setup nix flake devshell and .envrc

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Create nix expressions from bun lockfiles";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustc
+          cargo
+          rustfmt
+          clippy
+          rust-analyzer
+          mold
+        ];
+
+        env = {
+          RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
+        };
+      };
+    });
+}
