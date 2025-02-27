a71d375a0f4bfac61fe3b8d17f492a85923bd61a
Luke Bailey <baileylu@tcd.ie>
2025-02-27 17:24:59 +0000

    Added readme and followed clippy suggestions

---
+++ README.md
@@ -0,0 +1,19 @@
+# Bun2Nix
+
+Convert [Bun](https://bun.sh/) (v1.2+) packages to [Nix](https://nixos.wiki/) expressions.
+
+## Installation
+
+## Usage
+
+`bun2nix` can be invoked as follows:
+
+```
+Usage: bun2nix [OPTIONS]
+
+Options:
+  -l, --lock-file <LOCK_FILE>      The Bun (v1.2+) lockfile to use to produce the Nix expression [default: ./bun.lock]
+  -o, --output-file <OUTPUT_FILE>
+  -h, --help                       Print help
+  -V, --version                    Print version
+```
--- src/main.rs
+++ src/main.rs
@@ -25,14 +25,14 @@ fn main() {
     let args = Args::parse();
 
     let lockfile = fs::read_to_string(&args.lock_file)
-        .unwrap_or_else(|_| panic!("Could not find lockfile at {}. Try changing the file path to point to one, or create one with `bun install` on a version of bun above v1.2. See https://bun.sh/docs/install/lockfile to find out more information about the textual lockfile.", args.lockfile));
+        .unwrap_or_else(|_| panic!("Could not find lockfile at {}. Try changing the file path to point to one, or create one with `bun install` on a version of bun above v1.2. See https://bun.sh/docs/install/lockfile to find out more information about the textual lockfile.", args.lock_file));
 
     let nix = convert_lockfile_to_nix_expression(lockfile).unwrap();
 
     match args.output_file {
         Some(output_file) => {
             let mut output = File::create(output_file).unwrap();
-            write!(output, "{}", nix);
+            write!(output, "{}", nix).unwrap();
         }
         None => println!("{}", nix),
     };
