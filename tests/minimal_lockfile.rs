ef3c011aa2065d2f602615b5e63faaaa643264a0
Luke Bailey <baileylu@tcd.ie>
2025-03-04 13:24:27 +0000

    Setup integration tests properly

--- tests/minimal_lockfile.rs
+++ tests/minimal_lockfile.rs
@@ -5,9 +5,10 @@ async fn test_parse_minimal_lockfile() {
     let lockfile = fs::read_to_string("./examples/minimal/bun.lock")
         .expect("Could not find example lockfile for integration test");
 
-    let parsed = bun2nix::convert_lockfile_to_nix_expression(lockfile).await;
+    let parsed = bun2nix::convert_lockfile_to_nix_expression(lockfile)
+        .await
+        .unwrap();
+    let correct_nix = fs::read_to_string("./examples/minimal/bun.nix").unwrap();
 
-    println!("parsed: {:#?}", parsed);
-
-    assert!(parsed.is_ok());
+    assert_eq!(parsed, correct_nix);
 }
--- tests/react_lockfile.rs
+++ tests/react_lockfile.rs
@@ -5,9 +5,10 @@ async fn test_parse_react_lockfile() {
     let lockfile = fs::read_to_string("./examples/react/bun.lock")
         .expect("Could not find example lockfile for integration test");
 
-    let parsed = bun2nix::convert_lockfile_to_nix_expression(lockfile).await;
+    let parsed = bun2nix::convert_lockfile_to_nix_expression(lockfile)
+        .await
+        .unwrap();
+    let correct_nix = fs::read_to_string("./examples/react/bun.nix").unwrap();
 
-    println!("parsed: {:#?}", parsed);
-
-    assert!(parsed.is_ok());
+    assert_eq!(parsed, correct_nix);
 }
