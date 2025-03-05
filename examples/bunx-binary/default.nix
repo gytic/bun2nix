de8c6a7138d5dd2ee1e52f40b99d77880268e282
Luke Bailey <baileylu@tcd.ie>
2025-03-05 15:45:27 +0000

    Created runnable binary example

---
+++ examples/bunx-binary/default.nix
@@ -0,0 +1,16 @@
+{bun2nix, ...}:
+bun2nix.mkBunDerivation {
+  name = "binary-exec-bun2nix-example";
+  version = "1.0.0";
+
+  src = ./.;
+
+  bunNix = ./bun.nix;
+
+  # Verify that the cowsay binary was installed as expected and is runnable
+  preBuild = ''
+    bunx cowsay
+  '';
+
+  index = ./index.ts;
+}
