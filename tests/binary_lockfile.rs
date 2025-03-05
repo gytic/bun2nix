7f2a3741b8b9a8d60c7fc147f40fb40efb7443cd
Luke Bailey <baileylu@tcd.ie>
2025-03-05 19:31:23 +0000

    Added binary example lockfile test

---
+++ tests/binary_lockfile.rs
@@ -0,0 +1,14 @@
+use std::fs;
+
+#[tokio::test]
+async fn test_parse_minimal_lockfile() {
+    let lockfile = fs::read_to_string("./examples/bunx-binary/bun.lock")
+        .expect("Could not find example lockfile for integration test");
+
+    let parsed = bun2nix::convert_lockfile_to_nix_expression(lockfile)
+        .await
+        .unwrap();
+    let correct_nix = fs::read_to_string("./examples/bunx-binary/bun.nix").unwrap();
+
+    assert_eq!(parsed, correct_nix);
+}
