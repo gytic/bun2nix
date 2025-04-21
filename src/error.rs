fb8cd878e94484e04e6b31364333f0456f6e544a
Luke Bailey <baileylu@tcd.ie>
2025-04-21 21:33:16 +0100

    Remove cache related code

--- src/error.rs
+++ src/error.rs
@@ -23,25 +23,11 @@ pub enum Error {
     NoJsoncValue,
     #[error("Missing @ for package name and version declaration. Make sure all versions in your bun lockfile are formatted properly or try deleting it and running `bun install` to produce a fresh one.")]
     NoAtInPackageIdentifier,
-    #[error("Error occurred in nix-prefetch command: {}.", 0)]
-    Prefetch(#[from] std::io::Error),
-    #[error("Prefetch command returned an error code. STDERR: {}", 0)]
-    PrefetchStderr(String),
-    #[error("Cache table did not have value for: {}", 0)]
-    CacheTable(String),
-    #[error("Error parsing UTF8 nix-prefetch stdout: {}.", 0)]
-    UTF8Parse(#[from] std::string::FromUtf8Error),
     #[error(
         "Unsupported lockfile version: '{}'. Consider updating your local package or contributing to `bun2nix` if this version hasn't been supported yet.",
         0
     )]
     UnsupportedLockfileVersion(u8),
-    #[error("Error connecting to database: '{}'", 0)]
-    DatabaseConnection(#[from] sqlx::Error),
-    #[error("Error migrating database: '{}'", 0)]
-    DatabaseMigration(#[from] sqlx::migrate::MigrateError),
-    #[error("Error joining tokio task: '{}'", 0)]
-    JoinError(#[from] tokio::task::JoinError),
     #[error("Failed to render template: '{}'", 0)]
     TemplateError(#[from] rinja::Error),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,44 +3,29 @@
 
 #![warn(missing_docs)]
 
-pub mod cache;
 pub mod error;
 pub mod lockfile;
 pub mod nix_expression;
 pub mod package;
 
-use std::path::PathBuf;
-
-pub use cache::Cache;
 pub use error::{Error, Result};
 pub use lockfile::Lockfile;
 use nix_expression::NixExpression;
-use package::FetchMany;
 pub use package::Package;
 use rinja::Template;
 
 /// # Convert Bun Lockfile to a Nix expression
 ///
 /// Takes a string input of the contents of a bun lockfile and converts it into a ready to use Nix expression which fetches the packages
-pub async fn convert_lockfile_to_nix_expression(
-    contents: String,
-    cache_location: Option<PathBuf>,
-) -> Result<String> {
+pub fn convert_lockfile_to_nix_expression(contents: String) -> Result<String> {
     let lockfile = contents.parse::<Lockfile>()?;
 
     if lockfile.lockfile_version != 1 {
         return Err(Error::UnsupportedLockfileVersion(lockfile.lockfile_version));
     };
 
-    let packages = lockfile.packages();
+    let mut packages = lockfile.packages();
+    packages.sort();
 
-    let mut fods = if let Some(location) = cache_location {
-        Cache::new(location).await?.fetch_packages(packages).await?
-    } else {
-        packages.fetch_many().await?
-    };
-
-    fods.sort();
-
-    Ok(NixExpression::new(fods).render()?)
+    Ok(NixExpression::new(packages)?.render()?)
 }
