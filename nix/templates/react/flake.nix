02dc747fd475b6a25b11bcd0fc5b9309f5cb07f7
Luke Bailey <baileylu@tcd.ie>
2025-04-28 21:21:02 +0100

    Move templates over to nix-systems instead of flake-utils to align with the style of the main repo

--- nix/templates/react/flake.nix
+++ nix/templates/react/flake.nix
@@ -3,10 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
 
     bun2nix.url = "github:baileyluTCD/bun2nix?tag=1.4.1";
     bun2nix.inputs.nixpkgs.follows = "nixpkgs";
+    bun2nix.inputs.systems.follows = "systems";
   };
 
   # Use the cached version of bun2nix from the garnix cli
@@ -24,30 +25,34 @@
   outputs =
     {
       nixpkgs,
-      flake-utils,
+      systems,
       bun2nix,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+    let
+      # Read each system from the nix-systems input
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
 
-        # Call our package with the bun2nix library functions
-        react-site = pkgs.callPackage ./default.nix {
+      # Access the package set for a given system
+      pkgsFor = eachSystem (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = eachSystem (system: {
+        # Produce a package for this template with bun2nix
+        default = pkgsFor.${system}.callPackage ./default.nix {
           inherit (bun2nix.lib.${system}) mkBunDerivation;
         };
-      in
-      {
-        packages.default = react-site;
+      });
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
+      devShells = eachSystem (system: {
+        default = pkgsFor.${system}.mkShell {
+          packages = with pkgsFor.${system}; [
             bun
+
             # Add the bun2nix binary to our devshell
             bun2nix.packages.${system}.default
           ];
         };
-      }
-    );
+      });
+    };
 }
