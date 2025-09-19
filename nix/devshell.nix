ca5797b06d28e296a920259acdbdabd1f70fdcac
aster <137767097+aster-void@users.noreply.github.com>
2025-09-19 23:36:55 +0900

    fix(devshell): attrs now properly merge

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -1,49 +1,48 @@
 { pkgs, ... }:
 pkgs.mkShell (
+  let
+    # Mold does not support MacOS
+    enableMold = !pkgs.stdenv.isDarwin;
+  in
   {
-    packages = with pkgs; [
-      # Rust dependencies
-      rustc
-      cargo
-      rustfmt
-      clippy
+    packages =
+      with pkgs;
+      [
+        # Rust dependencies
+        rustc
+        cargo
+        rustfmt
+        clippy
 
-      # Database
-      sqlx-cli
-      sqlite
+        # Database
+        sqlx-cli
+        sqlite
 
-      # SSL
-      pkg-config
-      openssl
+        # SSL
+        pkg-config
+        openssl
 
-      # Docs
-      mdbook
+        # Docs
+        mdbook
 
-      # Javascript dependencies
-      bun
-    ];
+        # Javascript dependencies
+        bun
+      ]
+      ++ pkgs.lib.optional enableMold pkgs.mold;
 
-    env = with pkgs; {
-      LD_LIBRARY_PATH = lib.makeLibraryPath [ openssl ];
-      DATABASE_URL = "sqlite://.cache/bun2nix";
-    };
+    env =
+      with pkgs;
+      {
+        LD_LIBRARY_PATH = lib.makeLibraryPath [ openssl ];
+        DATABASE_URL = "sqlite://.cache/bun2nix";
+      }
+      // lib.optionalAttrs enableMold {
+        RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
+      };
 
     shellHook = ''
       mkdir -p .cache
       touch .cache/bun2nix
     '';
   }
-  # Mold does not support MacOS
-  // (
-    with pkgs;
-    lib.optionalAttrs (!stdenv.isDarwin) {
-      packages = [
-        mold
-      ];
-
-      env = {
-        RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
-      };
-    }
-  )
 )
