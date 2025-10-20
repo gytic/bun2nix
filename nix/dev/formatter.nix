59e744f7286676c6deb520efcf51078a24f66454
Luke Bailey <baileylu@tcd.ie>
2025-10-20 17:41:43 +0100

    Exclude .envrc from shellcheck

--- nix/dev/formatter.nix
+++ nix/dev/formatter.nix
@@ -4,17 +4,23 @@
 
   perSystem.treefmt = {
     projectRootFile = "flake.nix";
+
+    settings.global.excludes = [
+      "*.envrc"
+      "*.envrc."
+    ];
+
     programs = {
       deadnix.enable = true;
+      mdformat.enable = true;
       nixfmt.enable = true;
-      statix.enable = true;
-      shfmt.enable = true;
-      shellcheck.enable = true;
-      rustfmt.enable = true;
       prettier.enable = true;
+      rustfmt.enable = true;
+      shellcheck.enable = true;
+      shfmt.enable = true;
+      statix.enable = true;
       toml-sort.enable = true;
       zig.enable = true;
-      mdformat.enable = true;
     };
   };
 }
