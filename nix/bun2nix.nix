6f15c9a0a273cd8d63036ef70193a0a2364ce83a
Luke Bailey <baileylu@tcd.ie>
2025-10-24 10:37:53 +0100

    Expose `bun2nix` as default package

--- nix/bun2nix.nix
+++ nix/bun2nix.nix
@@ -11,36 +11,39 @@ in
   perSystem =
     { pkgs, config, ... }:
     {
-      packages.bun2nix = pkgs.rustPlatform.buildRustPackage (
-        finalAttrs:
-        let
-          pkgInfo = rootConfig.cargoTOML.package;
-        in
-        {
-          pname = pkgInfo.name;
-          inherit (pkgInfo) version;
+      packages = rec {
+        bun2nix = pkgs.rustPlatform.buildRustPackage (
+          finalAttrs:
+          let
+            pkgInfo = rootConfig.cargoTOML.package;
+          in
+          {
+            pname = pkgInfo.name;
+            inherit (pkgInfo) version;
 
-          src = "${self}/programs/bun2nix";
+            src = "${self}/programs/bun2nix";
 
-          cargoLock = {
-            lockFile = "${finalAttrs.src}/Cargo.lock";
-          };
+            cargoLock = {
+              lockFile = "${finalAttrs.src}/Cargo.lock";
+            };
 
-          passthru = with config; {
-            mkDerivation = mkDerivation.function;
-            inherit (mkDerivation) hook;
-            fetchBunDeps = fetchBunDeps.function;
-          };
+            passthru = with config; {
+              mkDerivation = mkDerivation.function;
+              inherit (mkDerivation) hook;
+              fetchBunDeps = fetchBunDeps.function;
+            };
 
-          meta = {
-            description = "A fast rust based bun lockfile to nix expression converter.";
-            homepage = "https://github.com/baileyluTCD/bun2nix";
-            license = lib.licenses.mit;
-            maintainers = [ lib.maintainers.baileylu ];
-          };
+            meta = {
+              description = "A fast rust based bun lockfile to nix expression converter.";
+              homepage = "https://github.com/baileyluTCD/bun2nix";
+              license = lib.licenses.mit;
+              maintainers = [ lib.maintainers.baileylu ];
+            };
 
-        }
-      );
+          }
+        );
+        default = bun2nix;
+      };
     };
 
 }
