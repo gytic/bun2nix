180a8bb050614dd717d3ccf7111e774a1054ee48
Luke Bailey <baileylu@tcd.ie>
2025-10-24 13:44:22 +0100

    Return `nixConfig` in flake

--- flake.nix
+++ flake.nix
@@ -12,5 +12,17 @@
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://cache.garnix.io"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+    sandbox = "true";
+  };
+
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./nix);
 }
