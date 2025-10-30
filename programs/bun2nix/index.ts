9cb3b5335f7ac969194f5afc8296087d8ec353ab
Luke Bailey <baileylu@tcd.ie>
2025-10-30 20:41:14 +0000

    Use bun shebang + properly document exposed functions

--- programs/bun2nix/index.ts
+++ programs/bun2nix/index.ts
@@ -1,18 +1,27 @@
-#!/usr/bin/env node
+#!/usr/bin/env bun
 
 import { convert_lockfile_to_nix_expression } from "./bun2nix-wasm.js";
 
 import sade from "sade";
-import pkgJson from "./package.json";
+import pkgJson from "./package.json" with { type: "json" };
 
 const prog = sade("bun2nix", true);
 
+/** `bun2nix` command line options. */
 type Opts = {
+  /** String path to the lockfile to read in. */
   "lock-file": string;
+  /** Output file to write to - writes to stdout if undefined. */
   "output-file": string | undefined;
 };
 
-async function run(opts: Opts) {
+/**
+ * Generate a nix expression for a given bun lockfile
+ * Writes to stdout if `output-file` is not specified.
+ *
+ * @param {Opts} opts - An instance of bun2nix CLI options
+ */
+export async function generateNixExpression(opts: Opts): Promise<void> {
   const lock_file = Bun.file(opts["lock-file"]);
   const contents = await lock_file.text();
 
@@ -34,13 +43,15 @@ prog
     "-o, --output-file",
     "The output file to write to - if no file location is provided, print to stdout instead",
   )
-  .action((opts) => run(opts));
+  .action((opts) => generateNixExpression(opts));
 
 prog.parse(process.argv);
 
-/** # Convert Bun Lockfile to a Nix expression
+/**
+ * Convert Bun Lockfile to a Nix expression
  *
- * Takes a string input of the contents of a bun lockfile and converts it into a ready to use Nix expression which fetches the packages
+ * @param {string} contents - The contents of a bun lockfile
+ * @return {string} The generated nix expression
  */
 export function convertLockfileToNixExpression(contents: string): string {
   return convert_lockfile_to_nix_expression(contents);
