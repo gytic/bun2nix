83a491ca3a601a3820dd578d3449ace65a3cc88c
Luke Bailey <baileylu@tcd.ie>
2025-10-30 18:12:33 +0000

    Remove extra quote

--- programs/bun2nix/scripts/prepare.ts
+++ programs/bun2nix/scripts/prepare.ts
@@ -5,7 +5,7 @@ import { $ } from "bun";
 await $`bun build index.ts --no-bundle --outfile=dist/index.js`;
 await $`cp package.json ./dist`;
 
-const transpiled = await $`cat dist/index.js"`.text();
+const transpiled = await $`cat dist/index.js`.text();
 
 const with_shebang = `#!/usr/bin/env node
 
